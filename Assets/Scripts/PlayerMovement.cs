5897801439286f359394a76ea109c5f0112558b7
Adira Rana <diamondadira@gmail.com>
2026-04-10 12:12:35 -0700

    movement created
    i made a sprite that moves and started working on directional sprites

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    private float speedX, speedY;
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // horizontalInput and verticalInput added by Github Copilot
+        float horizontalInput = 0f;
+        float verticalInput = 0f;
+
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) verticalInput += 1f;
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) verticalInput -= 1f;
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) horizontalInput += 1f;
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) horizontalInput -= 1f;
+
+        speedX = horizontalInput * speed;
+        speedY = verticalInput * speed;
+        rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+}
